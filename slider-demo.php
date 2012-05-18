b8973528fc016ee52ec5821c9724ae069ebb6b37
Cody <cody.thompson22@gmail.com>
2012-05-18 03:22:07 -0700

    separated slider page from home page

--- slider-demo.php
+++ slider-demo.php
@@ -1,8 +1,6 @@
 <?php
 
 /*
- * main index page - index.php
- *
  * creates the webpage using the makePage function from page.php, and the
  * information below as parameters to that function.
  */
@@ -11,10 +9,10 @@ require_once("page.php");
 
 $title = "Web Demos";
 $headerFile = "demoHeader.php";
-$contentFile = null;
+$contentFile = "sliderContent.php";
 $footerFile = null;
-$styleSheets = array("styles/demoStyles.css");
-$scripts = null;
+$styleSheets = array("styles/demoStyles.css", "styles/sliderStyles.css");
+$scripts = array("scripts/slider.js", "scripts/onloadScripts.js");
 
 makePage($title, $headerFile, $contentFile, $footerFile, $styleSheets,
     $scripts);
